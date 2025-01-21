0a6bb99361294e4ac7adaa97b2bdef79c8fce88a
NDH <nghai2k@gnail.com>
2025-01-21 08:25:32 +0700

    fix

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -293,7 +293,7 @@ class UserController extends AdminController
 
     public function profile(Request $request){
         $user = $request->user();
-        $user->permissions = $user->roles->flatMap->permissions;
+        $user->permission = $user->roles->flatMap->permissions;
         return $this->success($user);
     }
 }
