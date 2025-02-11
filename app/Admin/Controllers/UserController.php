8115d3ed5d9aeba2fccf058b20415b61472e43fd
NDH <nghai2k@gnail.com>
2025-02-11 16:26:29 +0700

    1

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -62,7 +62,7 @@ class UserController extends AdminController
             $user->value = $user->id;
             $user->label = $user->name;
         }
-        return $this->success(['data' => $users, 'pagination' => QueryHelper::pagination($request, $records)]);
+        return $this->success($users);
     }
     public function getUserRoles(Request $request)
     {
