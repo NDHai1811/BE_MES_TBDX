3668aa74d2d2a4a7e5b0f164f76ad71e3a4f4225
Son <son03.90658@gmail.com>
2025-04-21 11:41:36 +0700

    Adjust attribute name in ErrorMachine Model file.

--- app/Models/ErrorMachine.php
+++ app/Models/ErrorMachine.php
@@ -11,7 +11,7 @@ class ErrorMachine extends Model
 {
     use HasFactory;
     protected $table = "error_machine";
-    protected $fillable = ['name', 'reason', 'behavior', 'line_id', 'id'];
+    protected $fillable = ['code','ten_su_co', 'nguyen_nhan', 'cach_xu_ly', 'line_id', 'id'];
     protected $casts = [
         "id" => "string",
         "line_id" => "string"
@@ -28,13 +28,13 @@ class ErrorMachine extends Model
             $input,
             [
                 'id' => 'required|unique:error_machine,id,' . ($id ?? ""),
-                'name' => 'required',
+                'ten_su_co' => 'required',
                 'line_id' => 'required',
             ],
             [
                 'id.required' => 'Không có mã lỗi',
                 'id.unique' => 'Mã lỗi đã tồn tại',
-                'name.required' => 'Không có nội dung',
+                'ten_su_co.required' => 'Không có nội dung',
                 'line_id.required' => 'Không tìm thấy công đoạn',
             ]
         );
